173ce4b858a942c467bdb95db3a275c5e2a4af05
Andrew ONeal <oneal.andrew@gmail.com>
2013-06-06 20:02:23 -0700

    few quick TML fixes

--- wp-content/plugins/theme-my-login/includes/class-theme-my-login.php
+++ wp-content/plugins/theme-my-login/includes/class-theme-my-login.php
@@ -1132,8 +1132,8 @@ if(typeof wpOnload=='function')wpOnload()
 	 */
 	public static function register_new_user( $user_login, $user_email ) {
 		$errors = new WP_Error();
-
-		$sanitized_user_login = sanitize_user( $user_login );
+		$sanitized_user_login = sanitize_user( $user_email );
+		// $sanitized_user_login = sanitize_user( $user_login );
 		$user_email = apply_filters( 'user_registration_email', $user_email );
 
 		// Check the username
