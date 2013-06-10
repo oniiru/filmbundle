3f5ed91e2150ad6d60ff8720028cf6fbf6fe1a98
Johan Steen <artstorm@gmail.com>
2013-06-10 22:35:10 +0700

    Responsive: Social buttons in the header.

--- wp-content/themes/new-proxy-child-theme/header.php
+++ wp-content/themes/new-proxy-child-theme/header.php
@@ -82,13 +82,13 @@
         </div>
 
         <!-- BEGIN #primary-nav -->
-        <nav id="navigation" role="navigation">
   		  <div class="littlesocial">
     		<div class="fb-like" style="margin-right:20px;" data-href="http://facebook.com/filmbundle" data-send="false" data-layout="button_count" data-width="10" data-show-faces="false"></div>
     		<div style="position:relative;display:inline-block"><a href="https://twitter.com/FilmBundle" class="twitter-follow-button" data-show-count="false" data-show-screen-name="false">Follow @FilmBundle</a>
     		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
     	</div>
   </div>
+        <nav id="navigation" role="navigation">
           <?php
             if(has_nav_menu('primary-menu')){
               wp_nav_menu(array(
