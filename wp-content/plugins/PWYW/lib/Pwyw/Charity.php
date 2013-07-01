50308fbef09185f4ce4664e4fe8e68bded7e1531
Johan Steen <artstorm@gmail.com>
2013-07-01 15:45:08 +0700

    Initial commit.

---
+++ wp-content/plugins/PWYW/lib/Pwyw/Charity.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Class to handle charities.
+ */
+class Pwyw_Charity
+{
+    /** Holds the data for the charity instance */
+    public $id = null;
+    public $bundle_id = null;
+    public $title;
+    public $image;
+    public $embed;
+    public $description;
+
+    public function __construct($id = 0)
+    {
+        if ($id > 0) {
+            $this->id = $id;
+            if (!$this->load()) {
+                $this->id = null;
+            }
+        }
+    }
+
+    private function load()
+    {
+    }
+
+    public function save()
+    {
+    }
+
+
+    // -------------------------------------------------------------------------
+    // Static methods
+    // -------------------------------------------------------------------------
+
+    public static function create()
+    {
+    }
+
+    public static function delete()
+    {
+    }
+
+    public static function all()
+    {
+    }
+}
