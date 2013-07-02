717c34efbd2411aa5c8eb10946dae669953e8791
Johan Steen <artstorm@gmail.com>
2013-07-02 17:51:00 +0700

    Sets up the class to hold review entities.

--- wp-content/plugins/PWYW/lib/Pwyw/Review.php
+++ wp-content/plugins/PWYW/lib/Pwyw/Review.php
@@ -1,38 +1,42 @@
 <?php
 /**
- * Class to hold special feature entities.
+ * Class to hold review entities.
  */
-class Pwyw_Feature
+class Pwyw_Review
 {
-    /** Holds the data for the special feature instance */
+    /** Holds the data for the review instance */
     public $id = null;
     public $film_id = null;
+    public $review;
+    public $author;
+    public $publication;
     public $image;
-    public $title;
-    public $subtitle;
+    public $link;
 
     /**
-     * Save a special feature in the database.
+     * Save a review in the database.
      */
     public function save()
     {
         global $wpdb;
         $prefix = $wpdb->prefix.'pwyw_';
-        $table = $prefix.Pwyw_Database::FEATURES;
+        $table = $prefix.Pwyw_Database::REVIEWS;
 
         $data =  array(
-            'film_id'  => $this->film_id,
-            'image'    => $this->image,
-            'title'    => $this->title,
-            'subtitle' => $this->subtitle
+            'film_id'     => $this->film_id,
+            'review'      => $this->review,
+            'author'      => $this->author,
+            'publication' => $this->publication,
+            'image'       => $this->image,
+            'link'        => $this->link,
         );
 
         if ($this->id == null) {
-            // Insert new film
+            // Insert new review
             $wpdb->insert($table, $data, null);
             $res = ($wpdb->insert_id == 0) ? false : true;
         } else {
-            // Update existing film
+            // Update existing review
             $res = $wpdb->update($table, $data, array('id' => $this->id), null);
         }
         return $res;
@@ -44,30 +48,34 @@ class Pwyw_Feature
     // -------------------------------------------------------------------------
 
     /**
-     * Create a new special feature
+     * Create a new review
      */
     public static function create(
         $film_id,
+        $review,
+        $author,
+        $publication,
         $image,
-        $title,
-        $subtitle
+        $link
     ) {
-        $feature = new Pwyw_Feature;
-        $feature->film_id  = $film_id;
-        $feature->image    = $image;
-        $feature->title    = $title;
-        $feature->subtitle = $subtitle;
-        return $feature;
+        $review = new Pwyw_Review;
+        $review->film_id     = $film_id;
+        $review->review      = $review;
+        $review->author      = $author;
+        $review->publication = $publication;
+        $review->image       = $image;
+        $review->link        = $link;
+        return $review;
     }
 
     /**
-     * Delete a special feature
+     * Delete a review
      */
     public static function delete($id)
     {
         global $wpdb;
         $prefix = $wpdb->prefix.'pwyw_';
-        $table = $prefix.Pwyw_Database::FEATURES;
+        $table = $prefix.Pwyw_Database::REVIEWS;
 
         return $wpdb->query(
             $wpdb->prepare(
