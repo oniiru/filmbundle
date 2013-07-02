66c769be4892f2b6c0a67585127d4fb0f67be6bf
Johan Steen <artstorm@gmail.com>
2013-07-02 17:28:55 +0700

    Initial commit.

---
+++ wp-content/plugins/PWYW/lib/Pwyw/Feature.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Class to hold special feature entities.
+ */
+class Pwyw_Feature
+{
+    /** Holds the data for the special feature instance */
+    public $id = null;
+    public $film_id = null;
+    public $image;
+    public $title;
+    public $subtitle;
+}
