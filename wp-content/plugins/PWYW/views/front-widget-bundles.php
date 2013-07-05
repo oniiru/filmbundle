a112f24164009faf7a35832665c0e9c0edc9ad2b
Johan Steen <artstorm@gmail.com>
2013-07-05 22:15:23 +0700

    Adds markup and some basic CSS for getting foundation in place.

--- wp-content/plugins/PWYW/views/front-widget-bundles.php
+++ wp-content/plugins/PWYW/views/front-widget-bundles.php
@@ -1,7 +1,25 @@
-<div class="pwyw-bundle">
-    <a class="pwyw-bundle-show">Click to show</a>
+<div class='pwyw-bundle'>
+    <a class='pwyw-bundle-show'>Click to show</a>
+    <a class='pwyw-bundle-show'>Click to show</a>
+    <a class='pwyw-bundle-show'>Click to show</a>
+    <a class='pwyw-bundle-show'>Click to show</a>
 
-    <div class="pwyw-bundle-info">
+    <div class='pwyw-bundle-info'>
+        <div class='pwyw-info-header pwyw-clearfix'>
+            <div class='pwyw-previous'>
+                previous
+            </div>
+            <div class='pwyw-next'>
+                next
+            </div>
+            <div class='pwyw-tabs'>
+                <h3>film title</h3>
+                <a class='tab'>overview</a>
+                <a class='tab'>reviews</a>
+                <a class='tab'>special features</a>
+            </div>
+        </div>
+
         Etiam tristique condimentum neque sed aliquet. Mauris nunc elit, tincidunt id molestie sed, sollicitudin eget nibh. Vivamus porta leo sit amet ullamcorper bibendum. Curabitur sed dignissim mauris. Donec in iaculis est, pharetra vestibulum sapien. Vestibulum vel purus velit. Integer purus ligula, aliquam eu risus ac, consectetur consequat neque. Morbi aliquam lectus laoreet aliquet ullamcorper. In viverra, est sed dictum condimentum, sapien mauris cursus turpis, et viverra nibh neque in lacus. Suspendisse nec ipsum aliquet, blandit lorem et, tincidunt tellus. Suspendisse sollicitudin nibh id risus auctor, nec feugiat est mattis. Fusce in interdum dui. Duis eget turpis porttitor, tincidunt arcu nec, bibendum nisl. Vestibulum sed lectus nisl.
     </div>
 </div>
