aee25b464ddd00f476bace4a246cafc6aaa0e888
Johan Steen <artstorm@gmail.com>
2013-07-05 12:05:43 +0700

    Adds placeholding markup

--- wp-content/plugins/PWYW/views/front-widget-bundles.php
+++ wp-content/plugins/PWYW/views/front-widget-bundles.php
@@ -1,1 +1,8 @@
-we're here....
+<div class="pwyw-widget-bundle">
+    <a class="pwyw-widget-bundle-show">Click to show</a>
+
+    <div class="pwyw-widget-bundle-info">
+        Etiam tristique condimentum neque sed aliquet. Mauris nunc elit, tincidunt id molestie sed, sollicitudin eget nibh. Vivamus porta leo sit amet ullamcorper bibendum. Curabitur sed dignissim mauris. Donec in iaculis est, pharetra vestibulum sapien. Vestibulum vel purus velit. Integer purus ligula, aliquam eu risus ac, consectetur consequat neque. Morbi aliquam lectus laoreet aliquet ullamcorper. In viverra, est sed dictum condimentum, sapien mauris cursus turpis, et viverra nibh neque in lacus. Suspendisse nec ipsum aliquet, blandit lorem et, tincidunt tellus. Suspendisse sollicitudin nibh id risus auctor, nec feugiat est mattis. Fusce in interdum dui. Duis eget turpis porttitor, tincidunt arcu nec, bibendum nisl. Vestibulum sed lectus nisl.
+    </div>
+</div>
+
