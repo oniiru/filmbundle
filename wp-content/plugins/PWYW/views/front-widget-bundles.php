b6d313d50453c7dca39698162542a99ccc9840b7
Johan Steen <artstorm@gmail.com>
2013-07-06 07:54:17 +0700

    Adds additional markup for future styling.

--- wp-content/plugins/PWYW/views/front-widget-bundles.php
+++ wp-content/plugins/PWYW/views/front-widget-bundles.php
@@ -10,16 +10,18 @@
         Here would a description be inserted if we had one in the database...
     </p>
 
-    <?php
-    // Build the row of movie covers that slide down information
-    foreach ($bundle->films as $film) {
-        // Place the ones below average here
-        if ($film->rating === 'below') {
-            echo "<a class='pwyw-bundle-show' data-id='{$film->id}'>".
-                 "{$film->title}</a>";
+    <div class="shelf">
+        <?php
+        // Build the row of movie covers that slide down information
+        foreach ($bundle->films as $film) {
+            // Place the ones below average here
+            if ($film->rating === 'below') {
+                echo "<a class='pwyw-bundle-show' data-id='{$film->id}'>".
+                     "{$film->title}</a>";
+            }
         }
-    }
-    ?>
+        ?>
+    </div>
     
     <div class='pwyw-bundle-info'>
         <div class='pwyw-info-header pwyw-clearfix'>
@@ -37,7 +39,10 @@
             </div>
         </div>
 
-        Etiam tristique condimentum neque sed aliquet. Mauris nunc elit, tincidunt id molestie sed, sollicitudin eget nibh. Vivamus porta leo sit amet ullamcorper bibendum. Curabitur sed dignissim mauris. Donec in iaculis est, pharetra vestibulum sapien. Vestibulum vel purus velit. Integer purus ligula, aliquam eu risus ac, consectetur consequat neque. Morbi aliquam lectus laoreet aliquet ullamcorper. In viverra, est sed dictum condimentum, sapien mauris cursus turpis, et viverra nibh neque in lacus. Suspendisse nec ipsum aliquet, blandit lorem et, tincidunt tellus. Suspendisse sollicitudin nibh id risus auctor, nec feugiat est mattis. Fusce in interdum dui. Duis eget turpis porttitor, tincidunt arcu nec, bibendum nisl. Vestibulum sed lectus nisl.
+        <div class="pwyw-info-content">
+            <div class="pwyw-info-content-container">
+            Etiam tristique condimentum neque sed aliquet. Mauris nunc elit, tincidunt id molestie sed, sollicitudin eget nibh. Vivamus porta leo sit amet ullamcorper bibendum. Curabitur sed dignissim mauris. Donec in iaculis est, pharetra vestibulum sapien. Vestibulum vel purus velit. Integer purus ligula, aliquam eu risus ac, consectetur consequat neque. Morbi aliquam lectus laoreet aliquet ullamcorper. In viverra, est sed dictum condimentum, sapien mauris cursus turpis, et viverra nibh neque in lacus. Suspendisse nec ipsum aliquet, blandit lorem et, tincidunt tellus. Suspendisse sollicitudin nibh id risus auctor, nec feugiat est mattis. Fusce in interdum dui. Duis eget turpis porttitor, tincidunt arcu nec, bibendum nisl. Vestibulum sed lectus nisl.
+            </div>
+        </div>
     </div>
 </div>
-
