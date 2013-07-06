ccadc64cb0772f0fb621264615369fd90d92cd01
Johan Steen <artstorm@gmail.com>
2013-07-06 08:09:20 +0700

    Selecting a film now retrieves the film data to be used.

--- wp-content/plugins/PWYW/assets/javascripts/widget-bundle.js
+++ wp-content/plugins/PWYW/assets/javascripts/widget-bundle.js
@@ -1,7 +1,29 @@
 jQuery(document).ready(function($) {
 	$('.pwyw-bundle-show').click(function() {
 		var id = $(this).data('id');
-		console.log(id);
+		var film = getFilmById(id);
+		console.log(film);
 		$('.pwyw-bundle-info').slideToggle('fast');
 	});
+
+	// -------------------------------------------------------------------------
+	// Helpers
+	// -------------------------------------------------------------------------
+
+	/**
+	 * Lookup a film in the film object array.
+	 *
+	 * @returns object
+	 */
+	function getFilmById(id)
+	{
+		for (var key in pwyw_films) {
+			if(pwyw_films.hasOwnProperty(key)){
+        		if (pwyw_films[key].id == id) {
+        			return pwyw_films[key];
+        		}
+		    }
+		}
+		return false;
+	}
 });
