5e31a3e33885dd61ce427ab5fbc2fee22c782317
Johan Steen <artstorm@gmail.com>
2013-07-29 15:32:00 +0700

    Removes more redundant code.

--- wp-content/plugins/PWYW/views/front-widget-checkout.php
+++ wp-content/plugins/PWYW/views/front-widget-checkout.php
@@ -34,115 +34,40 @@ jQuery(document).ready(function($) {
         avg_price =  parseFloat(pwyw_data.payment_info.avg_price);
     }
 
-    var plot_data = [];
-    <?php
-    $j = 1;
+    $('.btn-success').click(function(){
+        user_alias = $('input[placeholder="username"]').val();
+        twit_alias = $('input[placeholder="twitterhandle"]').val();
 
-    if (!empty($pwyw_data['js_line'])):
-        foreach ($pwyw_data['js_line'] as $elems):
-
-            $string = '';
-            foreach ($elems as $el) {
-                $string .= "['{$el['title']}',{$el['allocate']}],";
-            }
-            $string = '[' . rtrim($string, ',') . ']';?>
-                var s<?php echo $j?> = <?php echo $string?>;
-                plot_data.push(s<?php echo $j?>);
-
-    <?php
-            $j++;
-        endforeach;
-    endif;
-    ?>
-
-//    console.log(linkedSliders);
-
-
-
-
-// jQuery(document).ready(function($) {
-//$(function(){
-
-
-        //if(pwyw_data.payment_info!=null){
-//            var interval = window.setInterval(function(){
-//                $.ajax({
-//                    type:'POST',
-//                    url:'<?php echo $pwyw->plugin_url?>ajax.php',
-//                    data:{bid:'<?php echo $pwyw_data['bundle']->id?>',_ajax_nonce: '<?php echo $nonce2?>'},
-//                    dataType:'json',
-//                    success: function(data){
-//                        if(typeof(data.error) == 'undefined'){
-//                            if(typeof(data.payment_info)!='undefined'&&data.payment_info!=null){
-//                                  update_view(data);                              
-//                            }
-//
-//                        }
-//                        else{console.log(data.error)}
-//                    }
-//                })
-//            },1000);
-       // }
-
-
-
+        var is_twitter = 0;
         
-        $('.btn_reset').click(function(){
-            var title = '';
-            var parent_cat;
-            var slider_class = $(this).parents('.modal').find('div.selector');
-            for(var key in pwyw_data.categories){
-                parent_cat = pwyw_data.categories[key];
-                title = parent_cat['info']['title'].toLowerCase();
-                if(key !== 3){
-                    if(slider_class.eq(0).attr('class').indexOf(title)){
-                        for(var k in parent_cat['sub']){
-                            $('#'+title+'_'+k+'_inp').val(parent_cat['sub'][k]['info']['val']);
-                            $('#slider_'+title+'_'+k).slider({value:parent_cat['sub'][k]['info']['val']});
-                        }
-                    }
-                }
-            }
-
-        })
+        if(twit_alias!=''){
+            alias = twit_alias;
+            is_twitter = 1
+        }else if(user_alias!=''){
+            alias = user_alias;
+        }
         
-        $('.btn-success').click(function(){
-            user_alias = $('input[placeholder="username"]').val();
-            twit_alias = $('input[placeholder="twitterhandle"]').val();
+        
+        $('input[name="alias"]').val(alias);
+        $('input[name="is_twitter"]').val(is_twitter);
 
-            var is_twitter = 0;
-            
-            if(twit_alias!=''){
-                alias = twit_alias;
-                is_twitter = 1
-            }else if(user_alias!=''){
-                alias = user_alias;
-            }
-            
-            
-            $('input[name="alias"]').val(alias);
-            $('input[name="is_twitter"]').val(is_twitter);
+          var btn = $('.buttonslidegroup').find('button.active');
+          if(!btn.length||$(btn).attr('id') == 'custom_price'){
+              c_price = $('input.custompricefield').val();
+          }else{
+             c_price = $(btn).val(); 
+          }
 
-              var btn = $('.buttonslidegroup').find('button.active');
-              if(!btn.length||$(btn).attr('id') == 'custom_price'){
-                  c_price = $('input.custompricefield').val();
-              }else{
-                 c_price = $(btn).val(); 
-              }
-   
-              $('input[name="c_price"]').val(c_price);
+          $('input[name="c_price"]').val(c_price);
 
-              if(c_price >= 0.01){
-                  check = true;
-              }
-                
-              $('#bundle_checkout').submit();
-              return false;  
+          if(c_price >= 0.01){
+              check = true;
+          }
             
-        });
-    // });
-     
-
+          $('#bundle_checkout').submit();
+          return false;  
+        
+    });
 });
 </script>
 
@@ -198,57 +123,24 @@ jQuery(document).ready(function($) {
                 <div class="statsmiddle">
                     <h3>Top Contributors:</h3>
 <!--                    <ul>-->
-                                            <?php
-                                                $j = 1;
-                                                if(isset($pwyw_data['top'])&&!empty($pwyw_data['top'])): ?>
-                                                    <table>
-                                                   <?php foreach($pwyw_data['top'] as $user):?>
-                                                        <tr><td><?php echo $j?>. <?php echo $user->display_name ?> </td><td> <span class="top_price">$<?php echo $user->amount?></span></td></tr>
-                                                <?php $j++;
-                                                     endforeach; ?>
-                                                       </table> 
-                                                <?php endif;
-                                                 ?>
+                        <?php
+                            $j = 1;
+                            if(isset($pwyw_data['top'])&&!empty($pwyw_data['top'])): ?>
+                                <table>
+                               <?php foreach($pwyw_data['top'] as $user):?>
+                                    <tr><td><?php echo $j?>. <?php echo $user->display_name ?> </td><td> <span class="top_price">$<?php echo $user->amount?></span></td></tr>
+                            <?php $j++;
+                                 endforeach; ?>
+                                   </table> 
+                            <?php endif;
+                             ?>
 <!--                    </ul>-->
                 </div>
-                <div class="statsright">
-                    <h3>How do they stack up? </h3>
-                    <div id="chart3">
-                    </div>
-                    <div class="chartlegend">
-                                        <?php
-                                              $j = 1;
-                                              foreach($pwyw_data['categories'] as $key=>$cat_obj):
-                                                   
-                                                   if($key != 3):?>
-                                                    <?php
-                                                        $smalltitle = strtolower($cat_obj['info']['title']);
-                                                        switch($key){
-                                                              case 1:
-                                                                  $ctitle = 'Films';
-                                                              break;
-                                                              case 2:
-                                                                  $ctitle = 'Charities';
-                                                              break;
 
-                                                              }
-                                                        ?>
 
-                                                    <h3><?php echo $ctitle ?></h3>
-                                                    <?php foreach ($cat_obj['sub'] as $key_s => $sub): ?>
-                                                        <div class="legenditem">
-                                                            <div class="legendcolor lc<?php echo $j ?>"></div>
-                                                            <div class="legendtitle"><?php echo $sub['info']['title'] ?></div>
-                                                        </div>
-                                                    <?php $j++;
-                                                        endforeach; ?>
-                                                <?php endif; ?>
-
-                                            <?php endforeach;?>
-                    </div>
-                </div>
+  
             </div>
-                    <?php //endif; ?>
+  
             <h2>Purchase</h2>
             <div class="step1">
                 <div class="buttonslidegroup">
@@ -282,23 +174,21 @@ jQuery(document).ready(function($) {
                         </ul>
                     </div>  
                     <div class="conteneur">
-                                            <?php foreach($pwyw_data['categories'] as $key=>$cat_obj): ?>
-                                                <div id="slider_<?php echo strtolower($cat_obj['info']['title']) ?>" value="<?php echo $cat_obj['info']['val'] ?>" class="linked3 selector inactive sliderhome"></div>
-                                            <?php endforeach;?>
-
+                        <?php foreach($pwyw_data['categories'] as $key=>$cat_obj): ?>
+                            <div id="slider_<?php echo strtolower($cat_obj['info']['title']) ?>" value="<?php echo $cat_obj['info']['val'] ?>" class="linked3 selector inactive sliderhome"></div>
+                        <?php endforeach;?>
                     </div>​ 
                     <div class="divedeeper input-append">
                         <ul>
-                                                    <?php foreach($pwyw_data['categories'] as $key=>$cat_obj): ?>
-                                                        <li id="mcat_<?php echo $key ?>" class="mcat">
-                                                            <input id='<?php echo strtolower($cat_obj['info']['title']) ?>_inp' name="categories[<?php echo $key ?>]" type="text" value="<?php echo $cat_obj['info']['val'] ?>" class="percent">
-                                                            <span class="add-on">%</span>
-                                                            <?php if($key !=3):?>
-                                                                <a class="btn btn-info btn-small" href="#<?php echo strtolower($cat_obj['info']['title']) ?>_modal" data-toggle="modal" type="button">Want more control?</a>
-                                                            <?php endif; ?>
-                                                        </li>
-                                                    <?php endforeach;?>
-
+                            <?php foreach($pwyw_data['categories'] as $key=>$cat_obj): ?>
+                                <li id="mcat_<?php echo $key ?>" class="mcat">
+                                    <input id='<?php echo strtolower($cat_obj['info']['title']) ?>_inp' name="categories[<?php echo $key ?>]" type="text" value="<?php echo $cat_obj['info']['val'] ?>" class="percent">
+                                    <span class="add-on">%</span>
+                                    <?php if($key !=3):?>
+                                        <a class="btn btn-info btn-small" href="#<?php echo strtolower($cat_obj['info']['title']) ?>_modal" data-toggle="modal" type="button">Want more control?</a>
+                                    <?php endif; ?>
+                                </li>
+                            <?php endforeach;?>
                         </ul>
                     </div>
                 </div>
@@ -306,7 +196,6 @@ jQuery(document).ready(function($) {
             </div>
         </div>
     </div>
-
 
 <!-- Slider 1 Modal -->
        <?php foreach($pwyw_data['categories'] as $key=>$cat_obj):  ?>
