572f8ababe8fd6112658ba73f8087423c1afd800
Johan Steen <artstorm@gmail.com>
2013-08-19 21:09:50 +0700

    Makes the class a singleton.

--- wp-content/plugins/PWYW/lib/Pwyw/Checkout.php
+++ wp-content/plugins/PWYW/lib/Pwyw/Checkout.php
@@ -1,8 +1,27 @@
 <?php
 /**
- * Handle checkout of bundles and integration with EDD
+ * Handle checkout of bundles and integration with EDD.
  */
 class Pwyw_Checkout
 {
+    /** Holds the class instance */
+    protected static $instance = false;
 
+    /** Singleton class */
+    public static function getInstance()
+    {
+        if (!self::$instance) {
+            self::$instance = new self();
+            self::$instance->construct();
+        }
+        return self::$instance;
+    }
+
+    /** Singleton Constructor */
+    public function __construct() {}
+
+    /** Custom constructor */
+    private function construct()
+    {
+    }
 }
