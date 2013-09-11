1c353962c521c530ce109d428799b9cf3f6571c4
Johan Steen <artstorm@gmail.com>
2013-09-11 21:10:20 +0700

    Apply meta sortable for the film section.

--- wp-content/plugins/PWYW/views/films.php
+++ wp-content/plugins/PWYW/views/films.php
@@ -2,7 +2,7 @@
   jQuery(document).ready(function($) {
     var film_data = {
         action: 'pwyw_add_film',
-        array_id: <?php echo count($films); ?> 
+        array_id: <?php echo count($films); ?>
     };
 
     function addFilm() {
@@ -28,20 +28,22 @@
 <h2>Films</h2>
 
 <div id="films">
-  <?php
-    foreach ($films as $key => $film) {
-      $data = array(
-        'array_id' => $key,
-        'film' => $film,
-        'features' => Pwyw_Films::allFeatures($film->id),
-        'reviews' => Pwyw_Films::allReviews($film->id)
-      );
-      echo Pwyw_View::make('film', $data);
-    }
-  ?>
+  <div class="meta-box-sortables">
+    <?php
+      foreach ($films as $key => $film) {
+        $data = array(
+          'array_id' => $key,
+          'film' => $film,
+          'features' => Pwyw_Films::allFeatures($film->id),
+          'reviews' => Pwyw_Films::allReviews($film->id)
+        );
+        echo Pwyw_View::make('film', $data);
+      }
+    ?>
+  </div>
 </div>
 
 <div style="text-align: right;">
-  <?php submit_button('Save Changes', 'primary', 'submit', false); ?> 
+  <?php submit_button('Save Changes', 'primary', 'submit', false); ?>
   <a class="button-secondary" id="add_film">+ Add Film</a>
 </div>
