66b17e3f08079922b9d403e6cbdf562cee13f6a7
Johan Steen <artstorm@gmail.com>
2013-09-12 10:51:30 +0700

    Inject the sort values on each reorder.

--- wp-content/plugins/PWYW/assets/javascripts/pwyw-postbox.js
+++ wp-content/plugins/PWYW/assets/javascripts/pwyw-postbox.js
@@ -45,89 +45,15 @@ var postboxes;
                 helper: 'clone',
                 opacity: 0.65,
                 stop: function(e,ui) {
-                    if ( $(this).find('#dashboard_browser_nag').is(':visible') && 'dashboard_browser_nag' != this.firstChild.id ) {
-                        $(this).sortable('cancel');
-                        return;
-                    }
-
-                    postboxes.save_order(page);
+                    var sortIDs = $('.meta-box-sortables').sortable('toArray');
+                    var sort = 0;
+                    _.each(sortIDs, function(id) {
+                        $('#'+id+'_sort').val(sort);
+                        sort++;
+                    });
                 },
-                receive: function(e,ui) {
-                    if ( 'dashboard_browser_nag' == ui.item[0].id )
-                        $(ui.sender).sortable('cancel');
-
-                    postboxes._mark_area();
-                }
             });
-
-            if ( isMobile ) {
-                $(document.body).bind('orientationchange.postboxes', function(){ postboxes._pb_change(); });
-                this._pb_change();
-            }
-
-            this._mark_area();
         },
-
-        save_order : function(page) {
-        //     var postVars, page_columns = $('.columns-prefs input:checked').val() || 0;
-
-        //     postVars = {
-        //         action: 'meta-box-order',
-        //         _ajax_nonce: $('#meta-box-order-nonce').val(),
-        //         page_columns: page_columns,
-        //         page: page
-        //     }
-        //     $('.meta-box-sortables').each( function() {
-        //         postVars["order[" + this.id.split('-')[0] + "]"] = $(this).sortable( 'toArray' ).join(',');
-        //     } );
-        //     $.post( ajaxurl, postVars );
-        },
-
-        _mark_area : function() {
-            // var visible = $('div.postbox:visible').length, side = $('#post-body #side-sortables');
-
-            // $('#dashboard-widgets .meta-box-sortables:visible').each(function(n, el){
-            //     var t = $(this);
-
-            //     if ( visible == 1 || t.children('.postbox:visible').length )
-            //         t.removeClass('empty-container');
-            //     else
-            //         t.addClass('empty-container');
-            // });
-
-            // if ( side.length ) {
-            //     if ( side.children('.postbox:visible').length )
-            //         side.removeClass('empty-container');
-            //     else if ( $('#postbox-container-1').css('width') == '280px' )
-            //         side.addClass('empty-container');
-            // }
-        },
-
-        // _pb_edit : function(n) {
-        //     var el = $('.metabox-holder').get(0);
-        //     el.className = el.className.replace(/columns-\d+/, 'columns-' + n);
-        // },
-
-        // _pb_change : function() {
-        //     var check = $( 'label.columns-prefs-1 input[type="radio"]' );
-
-        //     switch ( window.orientation ) {
-        //         case 90:
-        //         case -90:
-        //             if ( !check.length || !check.is(':checked') )
-        //                 this._pb_edit(2);
-        //             break;
-        //         case 0:
-        //         case 180:
-        //             if ( $('#poststuff').length ) {
-        //                 this._pb_edit(1);
-        //             } else {
-        //                 if ( !check.length || !check.is(':checked') )
-        //                     this._pb_edit(2);
-        //             }
-        //             break;
-        //     }
-        // },
 
         /* Callbacks */
         pbshow : false,
