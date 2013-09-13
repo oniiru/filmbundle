13df9b564edc68adbcdc904e0073742759e282cd
Johan Steen <artstorm@gmail.com>
2013-09-13 13:23:12 +0700

    Adds loading of tip script on films post type pages.

--- wp-content/plugins/PWYW/lib/Pwyw/Tip.php
+++ wp-content/plugins/PWYW/lib/Pwyw/Tip.php
@@ -7,6 +7,9 @@ class Pwyw_Tip
     /** Holds the class instance */
     protected static $instance = false;
 
+    /** Define class constants */
+    const SCRIPT_VERSIONS = '1.0';
+
     /** Singleton class */
     public static function getInstance()
     {
@@ -23,6 +26,25 @@ class Pwyw_Tip
     /** Custom constructor */
     private function construct()
     {
-        var_dump('tip initated');
+        add_action('wp_enqueue_scripts', array(&$this, 'scripts'));
+    }
+
+    /**
+     * Load scripts used by the film tipping.
+     */
+    public function scripts()
+    {
+        // Only load the script on the films post type
+        if (!is_singular('films')) {
+            return;
+        }
+
+        wp_enqueue_script(
+            'pwyw-tip-filmmaker',
+            plugins_url('/assets/javascripts/tip-filmmaker.js', Pwyw::FILE),
+            array('jquery'),
+            self::SCRIPT_VERSIONS,
+            true
+        );
     }
 }
